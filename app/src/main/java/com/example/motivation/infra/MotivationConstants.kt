45e253a048d323ea7bf925346fb411181fb684ed
IagoAntunes <iagoantunes.f@gmail.com>
2023-07-26 22:55:26 -0300

    Initial commit

---
+++ app/src/main/java/com/example/motivation/infra/MotivationConstants.kt
@@ -0,0 +1,21 @@
+package com.example.motivation.infra
+
+ class MotivationConstants private constructor() {
+
+    object  KEY{
+        const val USER_NAME = "user_name"
+    }
+
+     object FILTER{
+         const val ALL =1
+         const val HAPPY = 2
+         const val SUNNY = 3
+     }
+
+     object LANGUAGE{
+         const val PT = "pt"
+         const val ENGLISH = "en"
+         const val FRANCE = "fr"
+     }
+
+}
